9907d17c64c8ddd552776108b79a4255bac95cdd
nelson-david <larva.code15@gmail.com>
2025-01-05 11:48:44 +0100

    Added dynamic hero images samples

---
+++ src/app/data/roastedArray.tsx
@@ -0,0 +1,18 @@
+const roastedArray = [
+    require("../../assets/1.webp"),
+    require("../../assets/2.webp"),
+    require("../../assets/3.webp"),
+    require("../../assets/4.webp"),
+    require("../../assets/5.webp"),
+];
+
+const roastedHeaderImages = [
+    require("../../assets/roasted1.webp"),
+    require("../../assets/roasted2.webp"),
+    require("../../assets/roasted3.webp"),
+    require("../../assets/roasted4.webp"),
+    require("../../assets/roasted5.webp"),
+    require("../../assets/roasted6.webp"),
+];
+
+export { roastedArray, roastedHeaderImages };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+
 import Footer from "@/app/components/Footer";
 import styles from "@/app/styles/home.module.scss";
+import { roastedArray, roastedHeaderImages } from "./data/roastedArray";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+    const [currentHeaderImageIndex, setCurrentHeaderImageIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentHeaderImageIndex((prevIndex) => {
+                if (prevIndex === 5) {
+                    return 0;
+                }
+                return prevIndex + 1;
+            });
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="App">
             <div className={styles.wrapper}>
@@ -28,16 +48,82 @@ const Home = () => {
                     </div>
 
                     <div className={styles.ctaButtonWrapper}>
-                        <button>
-                            Roast me for Free{" "}
-                            <span>Depends on Availability</span>
-                        </button>
-                        <button>
-                            Guaranteed Roast at $22 <span>+Get Figma file</span>
-                        </button>
+                        <div>
+                            <button>
+                                Roast me for Free{" "}
+                                <span>Depends on Availability</span>
+                            </button>
+                            <button>
+                                Guaranteed Roast at $22{" "}
+                                <span>+Get Figma file</span>
+                            </button>
+                        </div>
                     </div>
 
-                    <div></div>
+                    <div className={styles.roastedCountWrapper}>
+                        <div>
+                            {roastedArray.map((img: any, index: number) => {
+                                return (
+                                    <Image
+                                        src={img}
+                                        alt={`${index}img`}
+                                        width={32}
+                                        height={32}
+                                        key={index}
+                                    />
+                                );
+                            })}
+                        </div>
+                        <p>Proudly Roasted 150+ designs</p>
+                    </div>
+
+                    <div className={styles.heroImagesWrapper}>
+                        <div>
+                            <div className={styles.macStyleWindow}>
+                                <div className={styles.macStyleWindowHeader}>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                                <Image
+                                    src={
+                                        roastedHeaderImages[
+                                            currentHeaderImageIndex
+                                        ]
+                                    }
+                                    alt={`.....`}
+                                    width={600}
+                                    height={290}
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <span>1</span>
+
+                            <div>
+                                <p>Submit your website</p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <span>2</span>
+
+                            <div>
+                                <p>Make a secure payment</p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <span>3</span>
+
+                            <div>
+                                <p>Get figma file in 1-2 days</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <Footer />
             </div>
